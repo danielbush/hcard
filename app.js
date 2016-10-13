24554e99185d10ab2122ebb6aa46a87d15be686d
Daniel Bush <dlb.id.au@gmail.com>
2016-10-13 23:38:59 +1100

    FEATURE: / now dynamically servers up index.html.

--- app.js
+++ app.js
@@ -6,14 +6,25 @@ const express = require('express'),
       path = require('path'),
       app = express(),
       publicPath = path.resolve(__dirname, 'public'),
+      cssPath = path.resolve(__dirname, 'public', 'css'),
+      imgPath = path.resolve(__dirname, 'public', 'img'),
       distPath = path.resolve(__dirname, 'dist');
 
 // Make React global because hCard assumes it.
 global.React = React;
 
 app.use(compression());
-app.use(express.static(publicPath));
+app.use('/css', express.static(cssPath));
+app.use('/img', express.static(imgPath));
 app.use(express.static(distPath));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(publicPath, 'index.html'), (err) => {
+    if (err) {
+      res.status(err.status).end();
+    }
+  });
+});
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
