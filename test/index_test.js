53ca043243761184ca1b8950c476007b3eaf7d01
Daniel Bush <dlb.id.au@gmail.com>
2016-10-13 22:14:56 +1100

    First commit.
    Not going to use express-generator.
    This is just hello world.

---
+++ test/index_test.js
@@ -0,0 +1,11 @@
+
+
+const chai = require('chai');
+const expect = chai.expect;
+
+describe('something', function () {
+  it('should do something', function () {
+    expect(true).to.equal(true);
+  });
+});
+
