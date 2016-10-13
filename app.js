84e94714fe010ba6a0b51ed7ad97dd1d0ad09365
Daniel Bush <dlb.id.au@gmail.com>
2016-10-13 23:07:00 +1100

    SETUP: adding compression and dist/ path.
    We would probably have nginx in front doing assets though.

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 
 const express = require('express'),
+      compression = require('compression'),
       path = require('path'),
       app = express(),
-      publicPath = path.resolve(__dirname, 'public');
+      publicPath = path.resolve(__dirname, 'public'),
+      distPath = path.resolve(__dirname, 'dist');
 
+app.use(compression());
 app.use(express.static(publicPath));
+app.use(express.static(distPath));
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
