5feb4fb453c7a7de169abd7e7d6d6d675b555179
Daniel Bush <dlb.id.au@gmail.com>
2016-10-13 23:21:38 +1100

    SETUP: made React global.

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 
 const express = require('express'),
+      React = require('react'),
+      ReactDOMServer = require('react-dom/server'),
       compression = require('compression'),
       path = require('path'),
       app = express(),
       publicPath = path.resolve(__dirname, 'public'),
       distPath = path.resolve(__dirname, 'dist');
+
+// Make React global because hCard assumes it.
+global.React = React;
 
 app.use(compression());
 app.use(express.static(publicPath));
